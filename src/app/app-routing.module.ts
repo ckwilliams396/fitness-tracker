61145fc555437ad2db9792879ee205e34a523d52
Williams, Cameron <cameron.williams@hawaiianair.com>
2022-06-17 14:54:34 -0700

    create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GoalsComponent } from './components/goals/goals.component';
+import { WorkoutsComponent } from './components/workouts/workouts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'goals-component', component: GoalsComponent},
+  {path: 'workouts-component', component: WorkoutsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GoalsComponent } from './components/goals/goals.component';
+import { ButtonComponent } from './components/button/button.component';
+import { WorkoutsComponent } from './components/workouts/workouts.component';
+import { AddGoalComponent } from './components/add-goal/add-goal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GoalsComponent,
+    ButtonComponent,
+    WorkoutsComponent,
+    AddGoalComponent
   ],
   imports: [
     BrowserModule,
